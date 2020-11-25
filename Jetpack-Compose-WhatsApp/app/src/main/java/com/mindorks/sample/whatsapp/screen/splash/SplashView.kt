08f979c0101e28a7f41b52d746a7bab1b42b1bc1
lh <im.lh@hotmail.com>
2020-11-25 17:37:08 +0800

    initial whatsapp and update to alpha07

---
+++ Jetpack-Compose-WhatsApp/app/src/main/java/com/mindorks/sample/whatsapp/screen/splash/SplashView.kt
@@ -0,0 +1,29 @@
+package com.mindorks.sample.whatsapp.screen.splash
+
+
+import androidx.compose.foundation.Box
+import androidx.compose.foundation.ContentGravity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.vectorResource
+import com.mindorks.sample.whatsapp.R
+import kotlinx.coroutines.delay
+
+private const val DELAY: Long = 2000
+
+@Composable
+fun SplashView(modifier: Modifier = Modifier, loadNextScreen: () -> Unit) {
+    Surface(color = colorResource(id = R.color.whatsapp_green)) {
+        Box(modifier = modifier.fillMaxSize(), gravity = ContentGravity.Center) {
+            LaunchedEffect(Unit) {
+                delay(DELAY)
+                loadNextScreen()
+            }
+            Image(asset = vectorResource(id = R.drawable.ic_whatsapp_logo))
+        }
+    }
+}
