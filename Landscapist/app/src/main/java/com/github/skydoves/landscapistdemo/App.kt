30529eff917c6e3aff933749d36a11d905304ece
lh <im.lh@hotmail.com>
2020-11-26 18:05:18 +0800

    read landscapist source code

--- Landscapist/app/src/main/java/com/github/skydoves/landscapistdemo/App.kt
+++ Landscapist/app/src/main/java/com/github/skydoves/landscapistdemo/App.kt
@@ -1,19 +1,3 @@
-/*
- * Designed and developed by 2020 skydoves (Jaewoong Eum)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 @file:Suppress("unused")
 
 package com.github.skydoves.landscapistdemo
@@ -25,17 +9,17 @@ import okhttp3.OkHttpClient
 
 class App : Application() {
 
-  override fun onCreate() {
-    super.onCreate()
+    override fun onCreate() {
+        super.onCreate()
 
-    val pipelineConfig =
-      OkHttpImagePipelineConfigFactory
-        .newBuilder(this, OkHttpClient.Builder().build())
-        .setDiskCacheEnabled(true)
-        .setDownsampleEnabled(true)
-        .setResizeAndRotateEnabledForNetwork(true)
-        .build()
+        val pipelineConfig =
+                OkHttpImagePipelineConfigFactory
+                        .newBuilder(this, OkHttpClient.Builder().build())
+                        .setDiskCacheEnabled(true)
+                        .setDownsampleEnabled(true)
+                        .setResizeAndRotateEnabledForNetwork(true)
+                        .build()
 
-    Fresco.initialize(this, pipelineConfig)
-  }
+        Fresco.initialize(this, pipelineConfig)
+    }
 }
--- Landscapist/app/src/main/java/com/github/skydoves/landscapistdemo/ui/MainActivity.kt
+++ Landscapist/app/src/main/java/com/github/skydoves/landscapistdemo/ui/MainActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Designed and developed by 2020 skydoves (Jaewoong Eum)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.github.skydoves.landscapistdemo.ui
 
 import android.os.Bundle
@@ -34,15 +18,16 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.ui.tooling.preview.Preview
 import com.github.skydoves.landscapistdemo.R
 import com.github.skydoves.landscapistdemo.model.MockUtil
 import com.github.skydoves.landscapistdemo.theme.DisneyComposeTheme
 import com.github.skydoves.landscapistdemo.theme.purple200
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             DisneyComposeTheme {
                 Scaffold(
@@ -54,23 +39,37 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun PosterAppBar() {
-    TopAppBar(
-            elevation = 6.dp,
-            backgroundColor = purple200,
-            modifier = Modifier.preferredHeight(58.dp)
-    ) {
-        Text(
-                modifier = Modifier
-                        .padding(8.dp)
-                        .align(Alignment.CenterVertically),
-                text = stringResource(R.string.app_name),
-                color = Color.White,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold
-        )
+    @Composable
+    fun PosterAppBar() {
+        TopAppBar(
+                elevation = 6.dp,
+                backgroundColor = purple200,
+                modifier = Modifier.preferredHeight(58.dp)
+        ) {
+            Text(
+                    modifier = Modifier
+                            .padding(8.dp)
+                            .align(Alignment.CenterVertically),
+                    text = stringResource(R.string.app_name),
+                    color = Color.White,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+            )
+        }
+    }
+
+    @Preview
+    @Composable
+    fun MainActivityPreview() {
+        DisneyComposeTheme {
+            Scaffold(
+                    backgroundColor = MaterialTheme.colors.primarySurface,
+                    topBar = { PosterAppBar() }
+            ) {
+                DisneyPosters(posters = MockUtil.getMockPosters())
+            }
+        }
     }
 }
+
