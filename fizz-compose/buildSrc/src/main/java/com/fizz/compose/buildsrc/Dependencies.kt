268f5d39e79e52d4cf985a83d6bf5d6ccb7259d0
lh <im.lh@hotmail.com>
2020-12-02 17:24:09 +0800

    modify dependencies

--- fizz-compose/buildSrc/src/main/java/com/fizz/compose/buildsrc/Dependencies.kt
+++ fizz-compose/buildSrc/src/main/java/com/fizz/compose/buildsrc/Dependencies.kt
@@ -14,29 +14,92 @@ object Libs {
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
         const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
+
+        object Coroutines {
+            private const val version = "1.4.0"
+            const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+            const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
+            const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
+        }
     }
 
-    object Coroutines {
-        private const val version = "1.4.0"
-        const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
-        const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
-        const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
+    object OkHttp {
+        private const val version = "4.7.2"
+        const val okhttp = "com.squareup.okhttp3:okhttp:$version"
+        const val logging = "com.squareup.okhttp3:logging-interceptor:$version"
     }
 
     object AndroidX {
+        const val appcompat = "androidx.appcompat:appcompat:1.2.0"
+        const val core = "androidx.core:core:1.5.0-alpha04"
         const val coreKtx = "androidx.core:core-ktx:1.5.0-alpha04"
+        const val palette = "androidx.palette:palette:1.0.0"
 
         object Compose {
             private const val version = "1.0.0-alpha07"
 
             const val runtime = "androidx.compose.runtime:runtime:$version"
+            const val runtimeLivedata = "androidx.compose.runtime:runtime-livedata:$version"
             const val foundation = "androidx.compose.foundation:foundation:$version"
             const val layout = "androidx.compose.foundation:foundation-layout:$version"
+
             const val ui = "androidx.compose.ui:ui:$version"
             const val material = "androidx.compose.material:material:$version"
-            const val animation = "androidx.compose.animation:animation:$version"
+            const val materialIconsExtended =
+                "androidx.compose.material:material-icons-extended:$version"
+
             const val tooling = "androidx.ui:ui-tooling:$version"
-            const val iconsExtended = "androidx.compose.material:material-icons-extended:$version"
+            const val animation = "androidx.compose.animation:animation:$version"
+            const val test = "androidx.compose.test:test-core:$version"
+            const val uiTest = "androidx.ui:ui-test:$version"
         }
+
+        object Navigation {
+            private const val version = "2.3.0"
+            const val fragment = "androidx.navigation:navigation-fragment-ktx:$version"
+            const val uiKtx = "androidx.navigation:navigation-ui-ktx:$version"
+        }
+
+        object Material {
+            private const val version = "1.2.0"
+            const val material = "com.google.android.material:material:$version"
+        }
+
+        object Room {
+            private const val version = "2.2.5"
+            const val runtime = "androidx.room:room-runtime:${version}"
+            const val ktx = "androidx.room:room-ktx:${version}"
+            const val compiler = "androidx.room:room-compiler:${version}"
+        }
+
+        object Lifecycle {
+            private const val version = "2.2.0"
+            const val extensions = "androidx.lifecycle:lifecycle-extensions:$version"
+            const val livedata = "androidx.lifecycle:lifecycle-livedata-ktx:$version"
+            const val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
+        }
+    }
+
+    object Hilt {
+        private const val version = "2.29.1-alpha"
+
+        const val gradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:$version"
+        const val android = "com.google.dagger:hilt-android:$version"
+        const val compiler = "com.google.dagger:hilt-compiler:$version"
+        const val testing = "com.google.dagger:hilt-android-testing:$version"
+
+        object AndroidX {
+            private const val version = "1.0.0-alpha02"
+
+            const val compiler = "androidx.hilt:hilt-compiler:$version"
+            const val viewModel = "androidx.hilt:hilt-lifecycle-viewmodel:$version"
+        }
+    }
+
+    object AssistedInjection {
+        private const val version = "0.5.2"
+
+        const val dagger = "com.squareup.inject:assisted-inject-annotations-dagger2:$version"
+        const val processor = "com.squareup.inject:assisted-inject-processor-dagger2:$version"
     }
 }
